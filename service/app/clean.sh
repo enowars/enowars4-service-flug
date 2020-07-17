2436d98b30e454f19c78fbd3601167f1d50bd46d
Aleks-dotcom <aleksandermundjar3@gmail.com>
2020-07-17 02:01:10 +0200

    all bunch of good changes

--- service/app/clean.sh
+++ service/app/clean.sh
@@ -1,7 +1,6 @@
 
-while :
-do
-	find ../users/ -maxdepth 1 -mmin +30 -type f -delete
-	find ../tickets/ -maxdepth 1 -mmin +30 -type f -delete
-	sleep 600
+while [ 1 ]; do
+        find /users -maxdepth 1 -mmin +30 -type f -delete;
+        find /tickets -maxdepth 1 -mmin +30 -type f -delete;
+        sleep 600;
 done
