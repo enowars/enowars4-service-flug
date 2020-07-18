5d334d969e915c8bade83b37b9932128d829e209
MMunier <marc@geomaps.de>
2020-07-18 02:09:39 +0200

    A bit more aggressive pruning

--- service/app/clean.sh
+++ service/app/clean.sh
@@ -1,6 +1,6 @@
 
 while [ 1 ]; do
-        find /users -maxdepth 1 -mmin +30 -type f -delete;
-        find /tickets -maxdepth 1 -mmin +30 -type f -delete;
-        sleep 600;
+        find /users -maxdepth 1 -mmin +20 -type f -delete;
+        find /tickets -maxdepth 1 -mmin +20 -type f -delete;
+        sleep 120;
 done
